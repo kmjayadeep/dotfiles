3cd80bcea4df9da3b64d87c901a9bbe40516aa2b
Jayadeep KM <kmjayadeep@gmail.com>
2022-02-22 21:02:21 +0530

    fix nvimtree
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -18,8 +18,13 @@ require('packer').startup(
     use 'airblade/vim-gitgutter'
 
     -- File manager
-    use 'kyazdani42/nvim-web-devicons'
-    use 'kyazdani42/nvim-tree.lua'
+    use {
+      'kyazdani42/nvim-tree.lua',
+      requires = {
+        'kyazdani42/nvim-web-devicons', -- optional, for file icon
+      },
+      config = function() require'nvim-tree'.setup {} end
+    }
 
     -- LSP
     use 'neovim/nvim-lspconfig'
